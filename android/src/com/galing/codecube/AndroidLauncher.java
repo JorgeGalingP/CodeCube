95643e0d8cef4a984e9bf4b256be91f40b581a13
Jorge Galindo Peña <jorgapedev@outlook.es>
2022-12-08 20:46:01 +0100

    Initial commit

---
+++ android/src/com/galing/codecube/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.galing.codecube;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.galing.codecube.CodeCube;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CodeCube(), config);
+	}
+}
