d72b7122270ae43008fbd243d1be23d125d53199
Jorge Galindo Peña <jorgapedev@outlook.es>
2022-12-09 23:32:42 +0100

    Removed unused assets and set screen dimensions

--- core/src/com/galing/codecube/CodeCube.java
+++ core/src/com/galing/codecube/CodeCube.java
@@ -1,31 +1,20 @@
 package com.galing.codecube;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class CodeCube extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    @Override
+    public void create() {
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(0, 0, 0, 1);
+    }
+
+    @Override
+    public void dispose() {
+    }
 }
