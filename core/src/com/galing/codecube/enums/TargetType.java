66711e04abd3acbfe13dcebf7122a044bef8378f
Jorge Galindo Peña <jorgapedev@outlook.es>
2023-01-15 21:45:47 +0100

    Added TargetType

---
+++ core/src/com/galing/codecube/enums/TargetType.java
@@ -0,0 +1,6 @@
+package com.galing.codecube.enums;
+
+public enum TargetType {
+    SUCCESS,
+    FAIL
+}
--- core/src/com/galing/codecube/objects/Target.java
+++ core/src/com/galing/codecube/objects/Target.java
@@ -2,21 +2,35 @@ package com.galing.codecube.objects;
 
 import com.badlogic.gdx.math.Vector2;
 import com.galing.codecube.AssetManager;
+import com.galing.codecube.enums.TargetType;
 
 public class Target extends Tile {
+
+    private TargetType type;
+
     public Target(Vector2 coordinate, String color) {
         super(coordinate);
 
         switch (color) {
             case "green":
+                setType(TargetType.SUCCESS);
                 setAtlasRegion(AssetManager.greenTarget);
                 break;
             case "red":
+                setType(TargetType.FAIL);
                 setAtlasRegion(AssetManager.redTarget);
                 break;
             case "white":
                 setAtlasRegion(AssetManager.whiteTarget);
                 break;
         }
+    }
+
+    public TargetType getType() {
+        return type;
+    }
+
+    public void setType(TargetType type) {
+        this.type = type;
     }
 }
