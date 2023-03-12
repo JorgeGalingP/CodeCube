25aee185d82690c9fb630814d44d1273736f8b20
Jorge Galindo Peña <jorgapedev@outlook.es>
2023-03-12 03:09:06 +0100

    Small modification on addInOutAction method

--- core/src/com/galing/codecube/objects/Tile.java
+++ core/src/com/galing/codecube/objects/Tile.java
@@ -65,7 +65,7 @@ public class Tile extends Actor {
         // perform in and out action
         addAction(Actions.sequence(
                 Actions.scaleTo(1.25f, 1.25f, .15f),
-                Actions.scaleTo(1f, 1f, .2f)));
+                Actions.scaleTo(.8f, .8f, .2f)));
     }
 
     public void addInOutPositionAction(Vector2 position) {
